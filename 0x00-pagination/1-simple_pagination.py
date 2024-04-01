72fb5a255e896c20d8b9c4327bb091b37ebd7a18
BelalGhonaim <belalpop1999@yahoo.com>
2024-04-01 23:54:29 +0200

    fixed file 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -11,7 +11,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     corresponding to the range of indexes to return in a list for those
     particular pagination parameters.
     """
-    return ((page - 1) * page_size, page * page_size)
+    return ((page - 1) * page_size, page_size * (page - 1) + page_size)
 
 
 
