3750ef5e2731b79e7d343e167ef808c269f9c83b
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-31 22:49:24 +0300

    fixed file 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -31,7 +31,7 @@ def get_locale() -> str:
 
 @app.route('/')
 def index() -> str:
-    """ deafult route 
+    """ deafult route
     Returns:
         str: template 2-index.html"""
     return render_template("2-index.html")
