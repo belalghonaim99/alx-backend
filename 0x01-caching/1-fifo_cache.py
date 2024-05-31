c08b4c7e7e856e0f9f0efa8d19769ae3451bb4c8
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-31 15:25:12 +0300

    fixed pycodestyle

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,6 +4,7 @@
 from collections import OrderedDict
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
     """
     FIFOCache class that inherits from BaseCaching and is a caching system
@@ -19,17 +20,17 @@ class FIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        
+
         # Check if the cache already contains the key
         if key in self.cache_data:
             # Remove the old key to update its position later
             del self.cache_data[key]
-        
+
         # If the cache is full, remove the first item
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             firstKey, _ = self.cache_data.popitem(last=False)
             print(f"DISCARD: {firstKey}")
-        
+
         # Add the new key-value pair to the cache
         self.cache_data[key] = item
 
