caa205ebab9eaa4ced37eb3dd2379c5eacbe1fa8
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-31 22:45:55 +0300

    add files

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Module for trying out Babel i18n """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Configuration Class for Babel"""
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ best match language
+
+    Returns:
+        str: best match language
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """deafult route
+    Returns:
+        str: template 3-index.html
+    """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
